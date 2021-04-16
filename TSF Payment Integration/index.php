670f29ee160b44a05899974efc81b158cb01f508
ORVELL FERREIRA <orvell8671@gmail.com>
2021-04-16 09:41:14 +0530

    Add files via upload

---
+++ TSF Payment Integration/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+
+</head>
+  <body>
+    <div class="topnav">
+        <a href="index.php"><img src="./images/logo.png" class="logo-image"></img></a>
+          <ul class="nav-area">
+            <li class="title">The Sparks Foundation</li>
+            <li>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</li>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#M&V">Mission & Vision</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+          </ul>
+    </div>
+    <div class="container1">
+        <img src="./images/image1.jpg"  style="width:100%;">
+          <div class="text-block1">
+              <h3>"Never Get Tired Of Doing Little Things To Others Sometimes Those Little Things Occupy The Biggest Parts
+          Of Their Hearts"</h3>
+          <form class="donate-btn">
+            <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_GxDeCKXaPYJeWn" async>
+             </script>
+          </form>
+          </div>
+    </div>
+
+    <div class="container2" id="about">
+        <h1>About Us</h1><hr>
+        <img src="./images/image2.jpg" style="width:100%;">
+          <div class="text-block2">
+
+            <h2>We remain aware of the mission</h2>
+            <p>We aim to collaborate, and help students through our programmes to inspire and integrate them, using innovative ways.</p>
+            <h2>We care for children and people</h2>
+            <p>We believe children are gifts of god and any service to them is service to god. We are committed to providing everyone and opportunity to learn, grow and help others grow.</p>
+            <h2>We treat everyone with respect</h2>
+            <p>We treat all with respect, dignity and honesty. We show respect by listening to and seeking to understand different points of views, being sensitive to cultural and religious diversity, and by working collaboratively with others to achieve common goals and a harmonious work culture.</p>
+            <h2>We collaborate to build partnerships</h2>
+            <p>We actively seek to partner with organizations with similar goals, individuals, and corporates to enhance the effectiveness of our programs and advance common interests.</p>
+            <h2>We honour the responsibilities entrusted to us</h2>
+            <p>We constantly seek opportunities and innovative ways to create value through the work that we do to further our mission.</p>
+
+          </div>
+    </div>
+    <div class="container3" id="M&V">
+        <h1>Mission & Vision</h1><hr>
+        <img src="./images/image3.jpg" style="width:100%;">
+          <div class="text-block3">
+            <h2>Vision</h2>
+            <p style="text-align:center">A world of enabled and connected little minds, building future.</p>
+            <h2>Mission</h2>
+            <p style="text-align:center">To inspire students, help them innovate and let them integrate to build the next generation humankind.</p>
+            <h2>Goals</h2>
+            <ul>
+              <li>Inspire</li>
+              <p>To inspire, motivate and encourage students to learn, create and help build a better society.</p>
+              <li>Innovate</li>
+              <p>To teach new ways of thinking, to innovate and solve the problems on their own.</p>
+              <li>Integrate</li>
+              <p>To let the students integrate, and help each other, learn from each other and do well together.</p>
+            </ul>
+          </div>
+    </div>
+    <div id="contact">
+        <h1>Contact Us</h1><hr>
+
+          <div class="flex-container">
+
+            <div class="flex-child">
+              <h4 >Contact</h4>
+              <i class="fas fa-phone-alt">&nbsp&nbsp</i><phone>+65-8402-8590</phone>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+              <i class="fas fa-envelope"></i>&nbsp&nbsp
+              <email><a href="mailto:info@thesparksfoundation.sg"> info@thesparksfoundation.sg</a></email>
+            </div>
+
+            <div class="flex-child">
+            <h4>Address</h4>
+            <i class="fas fa-map-marker-alt" ></i>&nbsp&nbsp<span> THE HANGAR, NUS ENTERPRISE<br>&nbsp&nbsp&nbsp&nbsp
+            21 HENG MUI KENG TERRACE, SINGAPORE, 119613</span>
+            </div>
+          </div>
+          <div class="container5" id="M&V">
+
+
+            <div class="text-block5">
+              <h2>Follow Us On ...</h2>
+                <div class="social">
+                  <a href="https://www.facebook.com/thesparksfoundation.info" class="fb" target="blank"><i class="fab fa-facebook"></i>&nbsp&nbsp&nbsp&nbsp</a>
+                  <a href="https://twitter.com/tsfsingapore" class="tweet" target="blank"><i class="fab fa-twitter"></i>&nbsp&nbsp&nbsp&nbsp</a>
+                  <a href="https://www.instagram.com/thesparksfoundation.info/" class="insta" target="blank"><i class="fab fa-instagram-square"></i>&nbsp&nbsp&nbsp&nbsp</a>
+                  <a href="https://www.linkedin.com/company/the-sparks-foundation/mycompany/" class="lnk" target="blank"><i class="fab fa-linkedin"></i></a>
+
+                </div>
+                <div class="footer">
+                  <hr>
+                  2021 © Made by Orvell Ferreira | All Rights Reserved | The Sparks Foundation
+                </div>
+
+              </div>
+
+          </div>
+
+    </div>
+
+  </body>
+
+</html>
